aa6361768cf2c257308423ffe04547d46bceeda0
Dmitry Kovalev <44893659+HolyClerk@users.noreply.github.com>
2022-07-08 20:34:58 +0500

    Реализация шейдерной программы

--- NordaProject/GameCore/Rendering/ShaderProgram.cs
+++ NordaProject/GameCore/Rendering/ShaderProgram.cs
@@ -6,16 +6,36 @@ internal class ShaderProgram
 {
     private const int IncorrectErrorCode = 0;
 
-    private int _vertexShader = 0;
+    private readonly int _vertexShader = 0;
+    private readonly int _fragmentShader = 0;
 
-    public ShaderProgram()
+    private readonly int _program = 0;
+
+    public ShaderProgram(string vertexShaderPath, string fragmentShaderPath)
     {
+        _vertexShader = CreateNewShader(ShaderType.VertexShader, vertexShaderPath);
+        _fragmentShader = CreateNewShader(ShaderType.FragmentShader, fragmentShaderPath);
 
+        _program = GL.CreateProgram();
+        GL.AttachShader(_program, _vertexShader);
+        GL.AttachShader(_program, _fragmentShader);
+
+        GL.LinkProgram(_program);
+
+        GL.GetProgram(_program, GetProgramParameterName.LinkStatus, out int resultCode);
+
+        if (resultCode == IncorrectErrorCode)
+        {
+            var infoLog = GL.GetProgramInfoLog(_program);
+            throw new Exception($"Произошла ошибка при компиляции программы шейдера \nID: {_program}, Code: {resultCode} \n{infoLog}");
+        }
+
+        DeleteShader(_vertexShader);
+        DeleteShader(_fragmentShader);
     }
-
-    private int CreateNewShader(ShaderType shaderType, string shaderPath)
+    private int CreateNewShader(ShaderType shaderType, string shaderPath = @"C:\Users\PHPpr\Documents\Development\MainProjects\Norda\NordaProject\GameCore\Rendering\Shaders\shader.vert")
     {
-        var shaderCode = File.ReadAllText(@"C:\Users\PHPpr\Documents\Development\MainProjects\Norda\NordaProject\GameCore\Rendering\Shaders\shader.vert");
+        var shaderCode = File.ReadAllText(shaderPath);
         var shaderID = GL.CreateShader(shaderType);
 
         GL.ShaderSource(shaderID, shaderCode);
@@ -26,10 +46,21 @@ internal class ShaderProgram
         if (resultCode == IncorrectErrorCode)
         {
             var infoLog = GL.GetShaderInfoLog(shaderID);
-            throw new Exception($"Произошла ошибка при компиляции шейдера ID: {shaderID}, Code: {shaderCode} \n{infoLog}");
+            throw new Exception($"Произошла ошибка при компиляции шейдера \nID: {shaderID}, Code: {shaderCode} \n{infoLog}");
         }
 
         return shaderID;
     }
-}
 
+    public void Run() => GL.UseProgram(_program);
+
+    public void Stop() => GL.UseProgram(0);
+
+    public void Destroy() => GL.DeleteProgram(_program);
+
+    private void DeleteShader(int shader)
+    {
+        GL.DetachShader(_program, shader);
+        GL.DeleteShader(shader);
+    }
+}
