5271182370bf8b7f882fed802d938d1023c8cacf
Dmitry Kovalev <44893659+HolyClerk@users.noreply.github.com>
2022-07-16 14:21:16 +0500

    Наброски EBO, IBuffer

--- NordaProject/GameCore/Rendering/Buffering/ElementBuffer.cs
+++ NordaProject/GameCore/Rendering/Buffering/ElementBuffer.cs
@@ -3,7 +3,61 @@ using System.Runtime.InteropServices;
 
 namespace NordaProject.GameCore.Rendering.Buffering;
 
-public class ElementBuffer
+public class ElementBuffer : IDisposable, IBuffer
 {
+    public ElementBuffer()
+    { 
+        
+    }
+
+    public int EBO 
+    { 
+        get; private set;
+    }
+
+    public bool IsBinded
+    {
+        get; private set;
+    }
+
+    public void InitializeDataStore<T>(T[] indices, BufferTarget target, BufferUsageHint hint = BufferUsageHint.StaticDraw)
+    where T : struct
+    {
+        if (indices.Length < 1)
+        {
+            throw new ArgumentException("Массив вершин должен содержать хотя-бы одну вершину.", nameof(indices));
+        }
+        GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+        GL.BufferData(target, (IntPtr)(indices.Length * Marshal.SizeOf(typeof(T))), indices, hint);
+    }
+
+    public void Bind()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UnBind()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete()
+    {
+        if (EBO == IBuffer.INCORRECT_CODE)
+        {
+            return;
+        }
+
+        UnBind();
+        GL.DeleteVertexArray(EBO);
+
+        EBO = IBuffer.INCORRECT_CODE;
+    }
+
+    public void Dispose()
+    {
+        Delete();
+        GC.SuppressFinalize(this);
+    }
 }
 
--- NordaProject/GameCore/Rendering/Buffering/IBuffer.cs
+++ NordaProject/GameCore/Rendering/Buffering/IBuffer.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenTK.Graphics.OpenGL4;
+namespace NordaProject.GameCore.Rendering.Buffering;
 
-namespace NordaProject.GameCore.Rendering.Buffering
+internal interface IBuffer
 {
-    internal interface IBuffer
-    {
-    }
+    protected const int INCORRECT_CODE = -1;
+    protected const int NULL_POINT = 0;
+
+    public abstract void Bind();
+
+    public abstract void UnBind();
+
+    public abstract void InitializeDataStore<T>(T[] vertices, BufferTarget target, BufferUsageHint hint = BufferUsageHint.StaticDraw) 
+        where T : struct;
+
+    public abstract void Delete();
 }
+
