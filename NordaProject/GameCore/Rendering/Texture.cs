0554c3d0495de693c4ca3358b5a10ceedf3894da
Dmitry Kovalev <44893659+HolyClerk@users.noreply.github.com>
2022-07-20 01:01:16 +0500

    Добавление класса Texture + подключение библиотеки ImageSharp

---
+++ NordaProject/GameCore/Rendering/Texture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK.Graphics.OpenGL4;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace NordaProject.GameCore.Rendering;
+
+public class Texture
+{
+}
+
