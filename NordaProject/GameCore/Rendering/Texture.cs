b16b0016f232389f4c5fb679301285348613bdad
Dmitry Kovalev <44893659+HolyClerk@users.noreply.github.com>
2022-07-20 22:16:38 +0500

    ImageSharp added

--- NordaProject/GameCore/Rendering/Texture.cs
+++ NordaProject/GameCore/Rendering/Texture.cs
@@ -10,5 +10,21 @@ namespace NordaProject.GameCore.Rendering;
 
 public class Texture
 {
+    public Texture()
+    {
+        Handle = GL.GenTexture();
+
+        Image<Rgba32> image = Image.Load<Rgba32>("");
+
+        //ImageSharp loads from the top-left pixel, whereas OpenGL loads from the bottom-left, causing the texture to be flipped vertically.
+        //This will correct that, making the texture display properly.
+        image.Mutate(x => x.Flip(FlipMode.Vertical));
+
+        //Use the CopyPixelDataTo function from ImageSharp to copy all of the bytes from the image into an array that we can give to OpenGL.
+        var pixels = new byte[4 * image.Width * image.Height];
+        image.CopyPixelDataTo(pixels);
+    }
+
+    public int Handle { get; private set; }
 }
 
